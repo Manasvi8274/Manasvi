d162a4fe328c25fd76d37904d16fe735cd563cd3
MANASVI <manasvi1238274@gmail.com>
2024-10-09 20:42:37 +0530

    Delete non_cp directory

--- non_cp/JAVA/lec1_2/a.java
+++
@@ -1,8 +0,0 @@
-package JAVA.lec1_2;
-
-public class a {
-    public static void main(String args []){
-        System.out.println("hello");
-        System.out.println("hello");
-    }
-}
--- non_cp/PYTHON/pygame_python/1.py
+++
@@ -1,6 +0,0 @@
-import pygame
-import time
-
-pygame.init()
-screen = pygame.display.set_mode((800,600))
-time.sleep(2)
